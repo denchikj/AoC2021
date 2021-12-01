5d8aea2e373ec3aa060b0cce0c712594529da4e8
denchikj <igoshevdenis9876@gmail.com>
2021-12-01 12:00:11 +0500

    Добавьте файлы проекта.

---
+++ AoC2021/part1.fs
@@ -0,0 +1,34 @@
+﻿module SonarSweep
+
+open System
+
+let input =
+    IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\input.txt"
+
+let example =
+    @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263"
+
+let isIncreases (x, y) = x < y
+
+let exp = 
+    example.Split("\n")
+    |> Seq.map int
+    |> Seq.pairwise
+    |> Seq.filter isIncreases
+    |> Seq.length
+
+let res1 =
+    input
+    |> Seq.map int
+    |> Seq.pairwise
+    |> Seq.filter isIncreases
+    |> Seq.length
