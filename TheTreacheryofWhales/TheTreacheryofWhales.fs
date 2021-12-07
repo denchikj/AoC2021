95ef5550914cf050726ab65b2cc3c04ce3e58472
denchikj <igoshevdenis9876@gmail.com>
2021-12-07 11:33:01 +0500

    TheTreacheryofWhales

---
+++ TheTreacheryofWhales/TheTreacheryofWhales.fs
@@ -0,0 +1,14 @@
+﻿module TheTreacheryofWhales
+
+open System
+
+let input =
+    (IO.File.ReadAllText "input.txt").Split(',') |> Array.map int |> Array.sort
+
+let solve = Array.sumBy(((-) input[input.Length / 2]) >> Math.Abs)
+solve input
+
+let avg = input |> Seq.averageBy decimal |> int 
+let fuel n = n*(n+1)/2
+let solve2 (x:int) = Array.sumBy(((-) x) >> Math.Abs >> fuel )
+solve2 avg input
