0922cc24322f58e135be4d7eba5685a3475dc5bc
denchikj <igoshevdenis9876@gmail.com>
2021-12-14 14:52:02 +0500

    edit ExtendedPolymerization part1 and add part2

--- ExtendedPolymerization/ExtendedPolymerization.fs
+++ ExtendedPolymerization/ExtendedPolymerization.fs
@@ -1,64 +1,114 @@
 ﻿module ExtendedPolymerization
 
-let input =
-    System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\input.txt"
+let input = System.IO.File.ReadAllLines $"input.txt"
 
-let parseRule (line: string) =
-    let [| src; dest |] = line.Split(" -> ")
-    (src |> Seq.toList, dest |> Seq.toList)
+let getData filename =
 
-let parse (input: string []) =
-    let template = input.[0] |> Seq.toList
+    let template =
+        input.[0]
+        |> Seq.pairwise
+        |> Seq.countBy id
+        |> Seq.map (fun (k, v) -> (k, int64 v))
+        |> Map.ofSeq
 
-    let rules =
-        input
-        |> Array.skip 2
-        |> Array.map parseRule
-        |> Map.ofArray
+    let elementCount =
+        input.[0]
+        |> Seq.countBy id
+        |> Seq.map (fun (k, v) -> (k, int64 v))
+        |> Map.ofSeq
 
-    (template, rules)
+    let insertionRules =
+        input.[2..]
+        |> Seq.choose
+            (fun str ->
+                match str.Split(" -> ") with
+                | [| l; r |] -> Some((l.[0], l.[1]), r.[0])
+                | _ -> None)
+        |> Map.ofSeq
 
-let apply rules (pair: char list) =
-    match rules |> Map.tryFind pair with
-    | None -> pair
-    | Some result ->
-        let [ a; b ] = pair
-        [ a ] @ result @ [ b ]
+    (template, elementCount, insertionRules)
 
-let processOne rules t =
-    let insertedPairs =
-        t
-        |> List.pairwise
-        |> List.map (fun (a, b) -> apply rules [ a; b ])
+let processData
+    (pairs: Map<(char * char), int64>)
+    (elementCount: Map<char, int64>)
+    (insertionRules: Map<(char * char), char>)
+    =
 
-    insertedPairs
-    |> List.map
-        (fun sublist ->
-            sublist
-            |> List.removeAt ((sublist |> List.length) - 1))
-    |> List.collect id
-    |> (fun t -> t @ [ insertedPairs |> List.last |> List.last ])
+    let tmp =
+        pairs
+        |> Map.toSeq
+        |> dict
+        |> System.Collections.Generic.Dictionary
 
-let (t, r) = parse input
+    let tmpCnt =
+        elementCount
+        |> Map.toSeq
+        |> dict
+        |> System.Collections.Generic.Dictionary
 
-let after10 =
-    t
-    |> processOne r
-    |> processOne r
-    |> processOne r
-    |> processOne r
-    |> processOne r
-    |> processOne r
-    |> processOne r
-    |> processOne r
-    |> processOne r
-    |> processOne r
+    pairs
+    |> Map.toSeq
+    |> Seq.iter
+        (fun ((a, b), times) ->
+            let c = insertionRules.[(a, b)]
 
-let sortedCounts =
-    after10
-    |> List.countBy id
-    |> List.sortByDescending snd
+            let ac =
+                match tmp.TryGetValue((a, c)) with
+                | (true, v) -> v
+                | _ -> 0
 
-let max = sortedCounts |> List.maxBy snd |> snd
-let min = sortedCounts |> List.minBy snd |> snd
-max - min
+            tmp.[(a, c)] <- (ac + times)
+
+            let cb =
+                match tmp.TryGetValue((c, b)) with
+                | (true, v) -> v
+                | _ -> 0
+
+            tmp.[(c, b)] <- (cb + times)
+
+            let ab =
+                match tmp.TryGetValue((a, b)) with
+                | (true, v) -> v
+                | _ -> 0
+
+            tmp.[(a, b)] <- (ab - times)
+
+            let cv =
+                match tmpCnt.TryGetValue(c) with
+                | (true, v) -> v
+                | _ -> 0
+
+            tmpCnt.[c] <- cv + times)
+
+    let pairs =
+        tmp
+        |> Seq.map (|KeyValue|)
+        |> Seq.filter (fun (_, v) -> v > 0L)
+        |> Map.ofSeq
+
+    let elementCount =
+        tmpCnt |> Seq.map (|KeyValue|) |> Map.ofSeq
+
+    (pairs, elementCount)
+
+let execute filename times =
+    let (template, elementCount, insertionRules) = getData filename
+
+    let folder (template, elementCount) _ =
+        processData template elementCount insertionRules
+
+    let cnt =
+        [ 1 .. times ]
+        |> List.fold folder (template, elementCount)
+        |> (fun (_, cnt) -> cnt |> Map.toSeq |> Seq.map snd)
+
+    let min = cnt |> Seq.min
+    let max = cnt |> Seq.max
+    max - min
+
+let solve filename = execute filename 10
+
+let solve2 filename = execute filename 40
+
+solve input
+solve2 input
